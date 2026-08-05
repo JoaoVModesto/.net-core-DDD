8aa717f1a63cf57e36ed54e9507f542d4e9ebeda
João Victor Modesto da Cruz <joaomodesto05@outlook.com>
2026-08-05 15:21:08 -0300

    criação de projetos

---
+++ Financeiro_2026/Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            Notificacoes = new List<Notifica>();
+        }
+
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string Mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifica> Notificacoes { get; set; }
+
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+
+        }
+
+    }
+}
